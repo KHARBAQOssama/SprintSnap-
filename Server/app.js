fb3372ae16ca9a8ba5de96d6dffe3c52523d9258
Ossama kharbaq <ossamakharbaq4@gmail.com>
2024-01-07 16:45:01 +0100

    working on the register functionality

--- Server/app.js
+++ Server/app.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
+const cookieParser = require("cookie-parser");
+const routes = require('./src/routes')
+require("dotenv").config();
+require("./src/config/database.config")();
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/',(req,res)=>{
     return res.status(400).json('hello world');
 })
+app.use("/api", routes);
 
 app.listen(3000, ()=>{
     console.log("running");
