ac7b58c79b125349adb869091faf234a74af2fb8
Ossama kharbaq <ossamakharbaq4@gmail.com>
2024-01-16 14:10:38 +0100

    Implement user controller for register method

--- Server/src/controllers/user.controller.js
+++ Server/src/controllers/user.controller.js
@@ -1,21 +1,35 @@
-const userService = require("../services/user.service");
+const { validationResult } = require("express-validator");
 
-async function registerUser(req, res) {
-    // return res.json(req.body)
-  const { first_name, last_name, email, password } = req.body;
-
-  try {
-    const user = await userService.registerUser(
-      first_name,
-      last_name,
-      email,
-      password
-    );
-    res.status(201).json({ message : "user registered successfully!", user });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Internal Server Error" });
+class UserController {
+  service;
+  constructor(UserService) {
+    this.service = UserService;
   }
+  registerUser = async (req, res) => {
+     const errors = validationResult(req);
+     if (!errors.isEmpty()) {
+       return res.status(400).json({ errors: errors.array() });
+     }
+     
+    const { first_name, last_name, email, password } = req.body;
+    try {
+      const existingUser = await this.service.getUserByEmail(email);
+      if (existingUser)
+        return res
+          .status(409)
+          .json({ message: "This email is already taken!" });
+      const user = await this.service.registerUser(
+        first_name,
+        last_name,
+        email,
+        password
+      );
+      res.status(201).json({ message: "user registered successfully!", user });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  };
 }
 
-module.exports = { registerUser };
+module.exports = UserController;
