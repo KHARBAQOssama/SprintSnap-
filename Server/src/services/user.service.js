4fd47c9f549355655e59d27439e5d0a864647681
Ossama kharbaq <ossamakharbaq4@gmail.com>
2024-01-16 14:10:58 +0100

    Integrate user service for register method

--- Server/src/services/user.service.js
+++ Server/src/services/user.service.js
@@ -1,13 +1,34 @@
-const User = require("../models/user.model");
-
-async function registerUser(first_name, last_name, email, password) {
-  try {
-    const user = new User({ first_name, last_name, email, password });
-    await user.save();
-    return user;
-  } catch (error) {
-    throw error;
+class UserService {
+  user;
+  model;
+  constructor(UserModel, Model) {
+    this.user = UserModel;
+    this.model = Model;
   }
+  registerUser = async (first_name, last_name, email, password) => {
+    try {
+      const user = new this.model({
+        first_name,
+        last_name,
+        email,
+        password,
+      });
+      await user.save();
+      return user;
+    } catch (error) {
+      throw error;
+    }
+  };
+  getUserByEmail = async (email) => {
+    try {
+      const user = await this.model.findOne({
+        email,
+      });
+      return user;
+    } catch (error) {
+      throw error;
+    }
+  };
 }
 
-module.exports = { registerUser };
+module.exports = UserService;
