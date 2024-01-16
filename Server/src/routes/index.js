614393a5e684355bf7b4f39965f86ab6396bbcd8
Ossama kharbaq <ossamakharbaq4@gmail.com>
2024-01-16 14:10:47 +0100

    Add route handling in index.js for register method

--- Server/src/routes/index.js
+++ Server/src/routes/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const userController = require('../controllers/user.controller')
+const authRouter = require('./auth.router')
 const router = express.Router();
 
-router.post('/register', userController.registerUser)
+router.use('/auth', authRouter);
 
 module.exports = router;
