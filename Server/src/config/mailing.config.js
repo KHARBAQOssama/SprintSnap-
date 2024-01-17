8bbe9c72b0369a9a18d2e3e7fac9e20333e8099e
Ossama kharbaq <ossamakharbaq4@gmail.com>
2024-01-17 14:36:26 +0100

    config the mailing using nodemailer

---
+++ Server/src/config/mailing.config.js
@@ -0,0 +1,13 @@
+const nodemailer = require("nodemailer");
+require('dotenv').config();
+
+const transport = nodemailer.createTransport({
+  host: "sandbox.smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASSWORD,
+  },
+});
+
+module.exports = transport;
