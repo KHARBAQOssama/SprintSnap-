e0676482dcb1a4847314d584b3f79ff5780b468c
Ossama kharbaq <ossamakharbaq4@gmail.com>
2024-01-31 16:01:58 +0100

    Update user service for improved functionality

--- Server/src/services/user.service.js
+++ Server/src/services/user.service.js
@@ -14,6 +14,7 @@ class UserService {
         password,
       });
       await user.save();
+
       return user;
     } catch (error) {
       throw error;
@@ -27,6 +28,35 @@ class UserService {
       return user;
     } catch (error) {
       throw error;
+    }
+  };
+  saveRefreshToken = async (userId, refreshToken) => {
+    try {
+      await this.model.updateOne(
+        { _id: userId },
+        { $set: { refreshToken: refreshToken } }
+      );
+      return true;
+    } catch (error) {
+      throw new Error("Refresh token did not save.");
+    }
+  };
+  verifyEmail = async (email) => {
+    try {
+      await this.model.updateOne({ email }, { $set: { verified: true } });
+      return true;
+    } catch (error) {
+      throw new Error("something went wrong verifying  your email");
+    }
+  };
+  updatePassword = async (password, email) => {
+    try {
+      let user = await this.getUserByEmail(email);
+      user.password = password;
+      await user.save();
+    } catch (error) {
+      console.log(error);
+      throw new Error("something went wrong updating your password");
     }
   };
 }
