68ec58c7d7ffb6dd1f80832477a5f5f0e8d1e048
Ossama kharbaq <ossamakharbaq4@gmail.com>
2024-01-31 16:01:58 +0100

    Update emailSender utility for better email handling

--- Server/src/utils/emailSender.js
+++ Server/src/utils/emailSender.js
@@ -1,12 +1,7 @@
 const transporter = require("../config/mailing.config");
 module.exports = async (messageOptions) => {
-    const message = {
-      from: `Allo Media ossamakahrbaq@dj.col`,
-      to: messageOptions.email,
-      subject: messageOptions.subject,
-      html: `<h1>hello</h1>`,
-    };
-  transporter.sendMail(message, (error, info) => {
+    
+  transporter.sendMail(messageOptions, (error, info) => {
     if (error) {
       console.log(error);
     }
