2c1a688ccf520a30a225728460b12dbc6e6bcfd6
Ossama kharbaq <ossamakharbaq4@gmail.com>
2024-01-31 16:01:59 +0100

    Add views for better user experience

---
+++ Client/src/views/Auth.jsx
@@ -0,0 +1,18 @@
+import Login from '../components/auth/Login'
+import Register from '../components/auth/Register'
+import ResetPassword from '../components/auth/ResetPassword'
+import ForgetPassword from '../components/auth/forgetPassword'
+import Logo from '../components/common/Logo'
+const Auth = () => {
+  return (
+    <div className='flex flex-col px-8 h-[100vh] w-[100vw] py-4 overflow-y-auto bg-light-auth-bg bg-no-repeat bg-cover bg-center gap-24'>
+        <Logo classItems={"h-[3.5em]"} containerClass={""} ></Logo>
+        {/* <Login></Login> */}
+        {/* <Register/> */}
+        {/* <ForgetPassword/> */}
+        {/* <ResetPassword/> */}
+    </div>
+  )
+}
+
+export default Auth
