53e968adb24e62a2a6412e4aac94840891661a32
Ossama kharbaq <ossamakharbaq4@gmail.com>
2024-02-01 14:32:15 +0100

    Update App component logic in App.jsx

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,11 +1,28 @@
 import "./App.css";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+import ResetPassword from "./components/auth/ResetPassword";
+import ForgetPassword from "./components/auth/forgetPassword";
 import Auth from "./views/Auth";
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Dashboard from "./views/Dashboard";
+
 
 function App() {
 
   return (
     <>
-      <Auth></Auth>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/auth" element={<Auth/>}>
+            <Route path="/auth/login" element={<Login/>}/>
+            <Route path="/auth/register" element={<Register/>}/>
+            <Route path="/auth/password/forget" element={<ForgetPassword/>}/>
+            <Route path="/auth/password/reset" element={<ResetPassword/>}/>
+        </Route>
+        <Route path="/dashboard" element={<Dashboard/>}></Route>
+      </Routes>
+    </BrowserRouter>
     </>
   );
 }
