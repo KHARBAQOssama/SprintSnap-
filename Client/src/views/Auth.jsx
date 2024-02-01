676b0f6f931e7969fac87e545ec426b8cf20bba2
Ossama kharbaq <ossamakharbaq4@gmail.com>
2024-02-01 14:32:15 +0100

    Update Auth view for authentication processes

--- Client/src/views/Auth.jsx
+++ Client/src/views/Auth.jsx
@@ -1,3 +1,4 @@
+import { Outlet } from 'react-router-dom'
 import Login from '../components/auth/Login'
 import Register from '../components/auth/Register'
 import ResetPassword from '../components/auth/ResetPassword'
@@ -7,6 +8,7 @@ const Auth = () => {
   return (
     <div className='flex flex-col px-8 h-[100vh] w-[100vw] py-4 overflow-y-auto bg-light-auth-bg bg-no-repeat bg-cover bg-center gap-24'>
         <Logo classItems={"h-[3.5em]"} containerClass={""} ></Logo>
+        <Outlet/>
         {/* <Login></Login> */}
         {/* <Register/> */}
         {/* <ForgetPassword/> */}
