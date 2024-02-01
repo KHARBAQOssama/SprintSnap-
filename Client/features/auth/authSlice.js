99c794463173e5e13245dc66787eec5e9966c7f6
Ossama kharbaq <ossamakharbaq4@gmail.com>
2024-02-01 14:32:14 +0100

    Update authentication slice for better state management

--- Client/features/auth/authSlice.js
+++ Client/features/auth/authSlice.js
@@ -1,17 +1,22 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import authService from "./authService";
 import {
+  Pending,
+  demandVerificationFulfilled,
+  loginFulfilled,
+  loginRejected,
+  logoutFulfilled,
   registerFulfilled,
-  registerPending,
   registerRejected,
 } from "./authCases";
 
-const userFormLocalStorage = localStorage.getItem("user");
-const user = userFormLocalStorage ? JSON.parse(localStorage.getItem("user")) : null;
 const initialState = {
-  user,
+  user: localStorage.getItem("user")
+    ? JSON.parse(localStorage.getItem("user"))
+    : null,
   isError: false,
   isSuccess: false,
+  logoutSuccess: false,
   isLoading: false,
   message: "",
 };
@@ -33,6 +38,30 @@ export const register = createAsyncThunk(
   }
 );
 
+export const login = createAsyncThunk("auth/login", async (user, thunkAPI) => {
+  try {
+    return await authService.login(user);
+  } catch (error) {
+    const message =
+      (error.response && error.response.data && error.response.data.message) ||
+      error.message ||
+      error.toString();
+    return thunkAPI.rejectWithValue(message);
+  }
+});
+
+export const demandVerification = createAsyncThunk("auth/demandverification", async (user, thunkAPI) => {
+  try {
+    return await authService.demandVerification(user);
+  } catch (error) {
+    const message =
+      (error.response && error.response.data && error.response.data.message) ||
+      error.message ||
+      error.toString();
+    return thunkAPI.rejectWithValue(message);
+  }
+});
+
 export const logout = createAsyncThunk("auth/logout", async () => {
   await authService.logout();
 });
@@ -46,17 +75,32 @@ export const authSlice = createSlice({
       state.isError = false;
       state.isLoading = false;
       state.isSuccess = false;
+      state.logoutSuccess = false;
     },
   },
   extraReducers: (builder) => {
     builder
-      .addCase(register.pending, (state) => registerPending(state))
+      .addCase(register.pending, (state) => {
+        Pending(state);
+        console.log("hiii");
+      })
       .addCase(register.fulfilled, (state, action) =>
         registerFulfilled(state, action)
       )
       .addCase(register.rejected, (state, action) =>
         registerRejected(state, action)
-      );
+      )
+      .addCase(login.pending, (state) => {
+        Pending(state);
+        console.log("hiii");
+      })
+      .addCase(login.fulfilled, (state, action) =>
+        loginFulfilled(state, action)
+      )
+      .addCase(login.rejected, (state, action) => loginRejected(state, action))
+      .addCase(logout.fulfilled, (state) => logoutFulfilled(state))
+      .addCase(demandVerification.fulfilled, (state) => demandVerificationFulfilled(state))
+      ;
   },
 });
 
