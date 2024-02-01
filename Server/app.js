9f0c46da53fcbecca9e9766ebbf728dc0afea1b6
Ossama kharbaq <ossamakharbaq4@gmail.com>
2024-02-01 14:32:16 +0100

    Update server application logic in app.js

--- Server/app.js
+++ Server/app.js
@@ -2,12 +2,20 @@ const express = require('express');
 const app = express();
 const cookieParser = require("cookie-parser");
 const routes = require('./src/routes')
+const cors = require("cors");
+
 require("dotenv").config();
 require("./src/config/database.config")();
 
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use("/api", routes);
 
