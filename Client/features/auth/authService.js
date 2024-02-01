9045b91b31d9f3bd7b92abeae6ebdebf31c5d6eb
Ossama kharbaq <ossamakharbaq4@gmail.com>
2024-02-01 14:32:14 +0100

    Update authentication service for enhanced functionality

--- Client/features/auth/authService.js
+++ Client/features/auth/authService.js
@@ -1,21 +1,40 @@
-import { toast } from "react-toastify"
 import api from "../../src/api"
 
 const register = async (userData) => {
     const response = await api.post('/auth/register',userData)
-    if(response.data){
+    console.log(response);
+    if(response.status == 201){
         localStorage.setItem('user',JSON.stringify(response.data.user))
     }
     return response.data
 }
-const logout = async () =>{
-    toast("Wow so easy!", {
-      type: "info",
-    });
+const login = async (userData) => {
+    const response = await api.post('/auth/login',userData)
+    console.log(response);
+    if(response.status == 201){
+        localStorage.setItem('user',JSON.stringify(response.data.user))
+    }
+    return response.data
 }
+
+const logout = async () =>{
+     const response = await api.post("/auth/logout");
+     console.log(response);
+     if (response.status == 200) {
+       localStorage.removeItem("user");
+     }
+     return response.data;
+}
+const demandVerification = async () => {
+  const response = await api.post("/auth/email/demandverification");
+  console.log(response);
+  return response.data;
+};
 const authService = {
     register,
-    logout
+    logout,
+    login,
+    demandVerification
 }
  
 export default authService
