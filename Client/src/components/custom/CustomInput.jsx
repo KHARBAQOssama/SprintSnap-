f6f24abaab5fa6bf154dc684735c73da440a578a
Ossama kharbaq <ossamakharbaq4@gmail.com>
2024-02-01 14:32:15 +0100

    Update CustomInput component for customizable input fields

--- Client/src/components/custom/CustomInput.jsx
+++ Client/src/components/custom/CustomInput.jsx
@@ -13,7 +13,7 @@ const CustomInput = ({ label, value, handleChange, type }) => {
           focus || value
             ? "top-4 text-xs text-blue-700"
             : "top-[50%] text-gray-400"
-        }  font-semibold`}
+        } `}
       >
         {label}
       </label>
@@ -22,7 +22,7 @@ const CustomInput = ({ label, value, handleChange, type }) => {
         id={id}
         onFocus={() => setFocus(true)}
         onBlur={() => setFocus(false)}
-        className={`w-full transition-all duration-300 focus:outline-none focus:border-blue-700 rounded-lg border-2 overflow-hidden border-gray-400 border-spacing-0 py-2 px-3 min-w-[300px] ${
+        className={`w-full transition-all duration-300 focus:outline-none focus:border-blue-700 rounded-lg bg-gray-100 overflow-hidden border-gray-400 border-spacing-0 py-2 px-3 min-w-[300px] ${
           focus || value ? "pt-6" : ""
         }`}
         value={value}
