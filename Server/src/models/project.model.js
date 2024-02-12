210a6ea1583fd47d772566849fe49e86cf449b95
Ossama kharbaq <ossamakharbaq4@gmail.com>
2024-02-12 23:51:16 +0100

    auth almost done, start projects part

--- Server/src/models/project.model.js
+++ Server/src/models/project.model.js
@@ -4,4 +4,4 @@ const bcrypt = require("bcryptjs");
 const projectSchema = new mongoose.Schema({});
 
 const Project = mongoose.model("Project", projectSchema);
-module.exports = User;
+module.exports = Project;
