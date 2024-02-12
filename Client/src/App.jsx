52b581396ce6fce36a8262f35dc69dac7668f26c
Ossama kharbaq <ossamakharbaq4@gmail.com>
2024-02-12 23:49:37 +0100

    add protected routes

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -4,25 +4,33 @@ import Register from "./components/auth/Register";
 import ResetPassword from "./components/auth/ResetPassword";
 import ForgetPassword from "./components/auth/forgetPassword";
 import Auth from "./views/Auth";
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dashboard from "./views/Dashboard";
-
+import RequireAuthRoute from "./routes/RequireAuthRoute";
+import { isAuthenticated } from "./middlewares";
 
 function App() {
-
   return (
     <>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/auth" element={<Auth/>}>
-            <Route path="/auth/login" element={<Login/>}/>
-            <Route path="/auth/register" element={<Register/>}/>
-            <Route path="/auth/password/forget" element={<ForgetPassword/>}/>
-            <Route path="/auth/password/reset" element={<ResetPassword/>}/>
-        </Route>
-        <Route path="/dashboard" element={<Dashboard/>}></Route>
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/auth" element={<Auth />}>
+            <Route path="/auth/login" element={<Login />} />
+            <Route path="/auth/register" element={<Register />} />
+            <Route path="/auth/password/forget" element={<ForgetPassword />} />
+            <Route path="/auth/password/reset" element={<ResetPassword />} />
+          </Route>
+          <Route element={<RequireAuthRoute />}>
+            <Route path="/dashboard" element={<Dashboard />}>
+              
+            </Route>
+          </Route>
+          {/* <RequireAuthRoute
+            path="/dashboard"
+            component={<Dashboard />}
+          ></RequireAuthRoute> */}
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
