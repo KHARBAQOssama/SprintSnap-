28608cfa910647df7f087fa20f2184cd412291af
Ossama kharbaq <ossamakharbaq4@gmail.com>
2024-02-24 18:24:45 +0100

    Update main application entry point in Client/src/main.jsx

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -8,10 +8,10 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <Provider store={store}>
       <App />
       <ToastContainer />
     </Provider>
-  </React.StrictMode>,
+  // </React.StrictMode>,
 )
