cb94d2ea5846fed7702ab53a91ae07d6c08faf00
Ossama kharbaq <ossamakharbaq4@gmail.com>
2024-03-03 19:46:45 +0100

    Update project controller logic in Server

--- Server/src/controllers/project.controller.js
+++ Server/src/controllers/project.controller.js
@@ -1,17 +1,23 @@
 const { validationResult } = require("express-validator");
+const teamControllerInstance = require("./team.controller");
 
 class ProjectController {
   service;
   constructor(Service) {
     this.service = Service;
   }
-  create = async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
+  getAll = async (req,res) =>{
     try {
-      const projectBody = { ...req.body, owner: req.user._id };
+      const projects = await this.service.getAll(req.user._id);
+      return res
+        .status(201)
+        .json({  projects });
+    } catch (error) {}
+  }
+  create = async (req, res) => {
+    const team = await teamControllerInstance.create(req);
+    try {
+      const projectBody = { ...req.body, owner: req.user._id, team: team._id };
       const project = await this.service.create(projectBody);
       return res.status(201).json({ message: "project created successfully",project });
     } catch (error) {}
