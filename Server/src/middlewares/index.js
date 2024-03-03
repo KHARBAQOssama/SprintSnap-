b6752ac4044439be527a319d3c7b86d8b4ad6727
Ossama kharbaq <ossamakharbaq4@gmail.com>
2024-03-03 19:46:46 +0100

    Update middleware configuration in Server

--- Server/src/middlewares/index.js
+++ Server/src/middlewares/index.js
@@ -1,4 +1,13 @@
-const requireAuth = require('./auth/requireAuth')
+const { validationResult } = require("express-validator");
+const requireAuth = require("./auth/requireAuth");
+const validationFailed = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+};
 module.exports = {
-    requireAuth
-}
+  requireAuth,
+  validationFailed
+};
