d99883b189e447b754aa65c07b08721f2f60c10d
Ossama kharbaq <ossamakharbaq4@gmail.com>
2024-03-03 19:46:45 +0100

    Update authentication middleware logic in Server

--- Server/src/middlewares/auth/requireAuth.js
+++ Server/src/middlewares/auth/requireAuth.js
@@ -1,4 +1,5 @@
 const jwt = require("jsonwebtoken");
+const User = require("../../models/user.model");
 
 module.exports = (req, res, next) => {
   const accessToken = req.cookies.accessToken;
@@ -7,9 +8,33 @@ module.exports = (req, res, next) => {
     return res.status(401).json({ message: "Unauthorized: Missing token" });
   }
 
-  jwt.verify(accessToken, process.env.JWT_SECRET, (err, user) => {
+  jwt.verify(accessToken, process.env.JWT_SECRET, async (err, user) => {
     if (err) {
-      return res.status(403).json({ message: "Forbidden: Invalid token" });
+      const user = await User.findById(req.cookies.userId);
+      jwt.verify(
+        user.refreshToken,
+        process.env.JWT_SECRET,
+        async (err, user) => {
+          if (err) {
+            user.refreshToken = null;
+            user.save();
+            return res
+              .status(403)
+              .json({ message: "Forbidden: Invalid token" });
+          }
+          console.log("hello");
+          const token = jwt.sign(user, process.env.JWT_SECRET, {
+            expiresIn: 900,
+          });
+
+          res.cookie("accessToken", token, {
+            httpOnly: true,
+            secure: true,
+            sameSite: "Strict",
+          });
+          req.user = user;
+        }
+      );
     }
 
     req.user = user;
