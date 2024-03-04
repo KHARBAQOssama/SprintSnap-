06dbad3f9ce1698358e373d6451be9d0a1fa79ad
Ossama kharbaq <ossamakharbaq4@gmail.com>
2024-03-04 16:48:15 +0100

    Update Test view

--- Client/src/views/Test.jsx
+++ Client/src/views/Test.jsx
@@ -12,6 +12,7 @@ import ListIcon from "../components/icons/Listicon";
 import ProfileIcon from "../components/icons/ProfileIcon";
 import CheckedBoxIcon from "../components/icons/CheckedBoxIcon";
 import Modal from "./Modal";
+import { motion } from "framer-motion";
 
 const Test = () => {
   const [open, setOpen] = useState();
@@ -95,7 +96,7 @@ const Test = () => {
           </div>
         </div>
       </aside>
-      <main className="min-w-[60em]">
+      {/* <main className="min-w-[60em]">
         <div className="bg-white p-6 border-b">
           <div className="flex gap-2 items-baseline">
             <div className="flex gap-2 items-center">
@@ -232,9 +233,226 @@ const Test = () => {
         closeModal={() => {
           setOpen(!open);
         }}
-      />
+      /> */}
+      <main className="min-w-[60em] flex-1 h-[100vh] bg-white">
+        <Board />
+      </main>
     </div>
   );
 };
 
 export default Test;
+
+const Board = () => {
+  const [cards, setCards] = useState(DEFAULT_CARDS);
+  return (
+    <div
+      className="flex h-full w-full gap-3
+  overflow-scroll p-12"
+    >
+      <Column
+        title="Backlog"
+        column="backlog"
+        headingColor="text-neutral-500"
+        cards={cards}
+        setCards={setCards}
+      />
+
+      <Column
+        title="TODO"
+        column="todo"
+        headingColor="text-yellow-200"
+        cards={cards}
+        setCards={setCards}
+      />
+
+      <Column
+        title="In progress"
+        column="doing"
+        headingColor="text-blue-200"
+        cards={cards}
+        setCards={setCards}
+      />
+
+      <Column
+        title="Complete"
+        column="done"
+        headingColor="text-emerald-200"
+        cards={cards}
+        setCards={setCards}
+      />
+    </div>
+  );
+};
+const Column = ({ title, headingColor, column, cards, setCards }) => {
+  const [active, setActive] = useState(false);
+  const filteredCards = cards.filter((c) => c.column === column);
+  const handleDragStart = (e, card) => {
+    e.dataTransfer.setData("cardId", card.id);
+  };
+  const handleDragOver = (e) => {
+    e.preventDefault();
+    highlightIndicator(e);
+    setActive(true);
+  };
+  const handleDragLeave = () => {
+    clearHighlights();
+    setActive(false);
+  };
+  const clearHighlights = (els) => {
+    const indicators = els || getIndicators();
+
+    indicators.forEach((i) => {
+      i.style.opacity = "0";
+    });
+  };
+
+  const highlightIndicator = (e) => {
+    const indicators = getIndicators();
+
+    clearHighlights(indicators);
+
+    const el = getNearestIndicator(e, indicators);
+
+    el.element.style.opacity = "1";
+  };
+
+  const getNearestIndicator = (e, indicators) => {
+    const DISTANCE_OFFSET = 50;
+
+    const el = indicators.reduce(
+      (closest, child) => {
+        const box = child.getBoundingClientRect();
+
+        const offset = e.clientY - (box.top + DISTANCE_OFFSET);
+
+        if (offset < 0 && offset > closest.offset) {
+          return { offset: offset, element: child };
+        } else {
+          return closest;
+        }
+      },
+
+      {
+        offset: Number.NEGATIVE_INFINITY,
+
+        element: indicators[indicators.length - 1],
+      }
+    );
+
+    return el;
+  };
+
+  const getIndicators = () => {
+    return Array.from(document.querySelectorAll(`[data-column="${column}"]`));
+  };
+  const handleDragEnd = (e) => {
+    clearHighlights();
+    setActive(false);
+    const cardId = e.dataTransfer.getData("cardId");
+    const indicators = getIndicators();
+    const { element } = getNearestIndicator(e, indicators);
+    const before = element.dataset.before || "-1";
+
+    if (before !== cardId) {
+      let copy = [...cards];
+      let cardToTransfer = copy.find((c) => c.id === cardId);
+      if (!cardToTransfer) return;
+      cardToTransfer = { ...cardToTransfer, column };
+
+      copy = copy.filter((c) => c.id !== cardId);
+      const moveToBack = before === "-1";
+      if (moveToBack) {
+        copy.push(cardToTransfer);
+      } else {
+        const insertAtIndex = copy.findIndex((el) => el.id === before);
+        if (insertAtIndex === undefined) return;
+        copy.splice(insertAtIndex, 0, cardToTransfer);
+      }
+      setCards(copy);
+    }
+  };
+  return (
+    <div className="w-56 shrink-0">
+      <div className="mb-3 flex items-center justify-between">
+        <h3 className={`font-medium ${headingColor}`}>{title}</h3>
+        <span className="rounded text-sm text-neutral-400">
+          {filteredCards.length}{" "}
+        </span>
+      </div>
+      <div
+        className={`h-full w-full transition-colors ${
+          active ? "Obg-neutral-800/50" : "bg-neutral-800/0"
+        }`}
+        onDrop={handleDragEnd}
+        onDragOver={handleDragOver}
+        onDragLeave={handleDragLeave}
+      >
+        {filteredCards.map((c) => {
+          return <Card handleDragStart={handleDragStart} key={c.id} {...c} />;
+        })}
+        <DropIndicator beforeId="-1" column={column} />
+      </div>
+    </div>
+  );
+};
+const Card = ({ title, id, column, handleDragStart }) => {
+  return (
+    <>
+      <DropIndicator beforeId={id} column={column} />
+      <motion.div
+        layout
+        layoutId={id}
+        onDragStart={(e) => handleDragStart(e, { title, id, column })}
+        draggable="true"
+        className="cursor-grab rounded border.
+  border-neutral-200 bg-neutral-300 p-3
+  active:cursor-grabbing"
+      >
+        <p className="text-sm text-neutral-800">{title}</p>
+      </motion.div>
+    </>
+  );
+};
+const DropIndicator = ({ beforeId, column }) => {
+  return (
+    <div
+      data-before={beforeId || "-1"}
+      data-column={column}
+      className="my-0.5 h-0.5 w-full bg-violet-400 opacity-0"
+    />
+  );
+};
+const DEFAULT_CARDS = [
+  { title: "Look into render bug in dashboard", id: "1", column: "backlog" },
+
+  { title: "SOX compliance checklist", id: "2", column: "backlog" },
+
+  { title: "[SPIKE] Migrate to Azure", id: "3", column: "backlog" },
+
+  { title: "Document Notifications service", id: "4", column: "backlog" },
+
+  {
+    title: "Research DB options for new microservice",
+    id: "5",
+    column: "todo",
+  },
+
+  { title: "Postmortem for outage", id: "6", column: "todo" },
+
+  { title: "Sync with product on Q3 roadmap", id: "7", column: "todo" },
+
+  {
+    title: "Refactor context providers to use Zustand",
+    id: "8",
+    column: "doing",
+  },
+
+  { title: "Add logging to daily CRON", id: "9", column: "doing" },
+
+  {
+    title: "Set up DD dashboards for Lambda listener",
+    id: "10",
+    column: "done",
+  },
+];
