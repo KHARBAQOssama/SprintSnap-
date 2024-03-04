b9e7ae099b6a3bf2bec02f5f71bb23d43b6f572d
Ossama kharbaq <ossamakharbaq4@gmail.com>
2024-03-04 16:48:14 +0100

    Update Logo component

--- Client/src/components/common/Logo.jsx
+++ Client/src/components/common/Logo.jsx
@@ -1,4 +1,4 @@
-const Logo = ({ containerClass, classItems, full }) => {
+const Logo = ({ containerClass, classItems }) => {
   return (
     <div className={`${containerClass} flex w-max gap-2 items-center`}>
       <img
@@ -6,11 +6,6 @@ const Logo = ({ containerClass, classItems, full }) => {
         src="/images/logo.png"
         alt="Sprint-Snap-Logo"
       ></img>
-      {full && (
-        <h1 className="text-[150%] w-max text-black font-semibold dark:text-gray-100">
-          SprintSnap
-        </h1>
-      )}
     </div>
   );
 };
