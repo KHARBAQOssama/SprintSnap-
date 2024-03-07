67617fbf19ffed59521bb618183128aaba04e9bc
Ossama kharbaq <ossamakharbaq4@gmail.com>
2024-03-07 13:47:23 +0100

    Update main component

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import {store} from '../app/store.js'
-import { Provider } from 'react-redux'
-import './index.css'
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { store } from "../app/store.js";
+import { Provider } from "react-redux";
+import "./index.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
-    <Provider store={store}>
+  <Provider store={store}>
+    <BrowserRouter>
       <App />
-      <ToastContainer />
-    </Provider>
+    </BrowserRouter>
+    <ToastContainer />
+  </Provider>
   // </React.StrictMode>,
-)
+);
