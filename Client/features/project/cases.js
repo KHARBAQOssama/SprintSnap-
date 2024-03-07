11598826bb800e61107bb48f73fdf296b6712d79
Ossama kharbaq <ossamakharbaq4@gmail.com>
2024-03-07 13:47:22 +0100

    Update project cases logic

--- Client/features/project/cases.js
+++ Client/features/project/cases.js
@@ -4,11 +4,29 @@ export const Pending = (state) => {
 export const getAllFulfilled = (state, action) => {
   state.isLoading = false;
   state.isSuccess = true;
-  state.projects = action.payload.projects;
+  state.projects = action.payload;
 };
 export const getAllRejected = (state, action) => {
-  state.message = action.payload;
+  if (action.payload.status == 401) {
+    state.status = action.payload.status;
+  } else {
+    state.message = action.payload.data.message;
+  }
   state.isLoading = false;
   state.isError = true;
   state.projects = [];
+};
+export const createProjectFulfilled = (state, action) => {
+  state.isLoading = false;
+  state.isSuccess = true;
+  state.projects = action.payload;
+};
+export const createProjectRejected = (state, action) => {
+  if (action.payload.status == 401) {
+    state.status = action.payload.status;
+  } else {
+    state.message = action.payload.data.message;
+  }
+  state.isLoading = false;
+  state.isError = true;
 };
