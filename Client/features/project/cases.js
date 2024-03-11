167bcebf3001804729a713948ae15c977d76fc58
Ossama kharbaq <ossamakharbaq4@gmail.com>
2024-03-11 17:01:30 +0000

    SPRIN-4: Update project creation cases logic

--- Client/features/project/cases.js
+++ Client/features/project/cases.js
@@ -20,19 +20,31 @@ export const getAllRejected = (state, action) => {
 };
 export const createProjectFulfilled = (state, action) => {
   console.log(action);
-  const project = {
-    _id: action.payload.project._id,
-    icon: action.payload.project.icon,
-    name: action.payload.project.name,
-  };
-  const projects = [...state.projects, project]
-  state.projects = projects
+  const projects = [...state.projects, action.payload.project];
+  state.projects = projects;
   state.isLoading = false;
   state.isSuccess = true;
-  toast.success(action.payload.message)
+  toast.success(action.payload.message);
 };
 export const createProjectRejected = (state, action) => {
   if (action.payload.status == 401) {
+    state.status = action.payload.status;
+  } else {
+    state.message = action.payload.data.message;
+  }
+  state.isLoading = false;
+  state.isError = true;
+};
+export const getProjectFulfilled = (state, action) => {
+  console.log(action);
+  state.activeProject = action.payload.project;
+  state.isLoading = false;
+  state.isSuccess = true;
+};
+export const getProjectRejected = (state, action) => {
+  console.log(action);
+  if (action.payload.status == 401) {
+    console.log("hello");
     state.status = action.payload.status;
   } else {
     state.message = action.payload.data.message;
