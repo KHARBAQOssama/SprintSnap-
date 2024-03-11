0d607f8484727e19bde47a88b696552167f97fb6
Ossama kharbaq <ossamakharbaq4@gmail.com>
2024-03-11 17:01:32 +0000

    SPRIN-4: Update utility functions for project creation

--- Client/src/utils/functions.js
+++ Client/src/utils/functions.js
@@ -14,3 +14,34 @@ export const randomColorGenerator = () => {
   }
   return color;
 };
+
+export const formatDate = (inputDateStr) => {
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  const date = new Date(inputDateStr);
+  const year = date.getFullYear();
+  const month = months[date.getMonth()];
+  const day = date.getDate();
+  const hours = date.getHours() % 12 || 12;
+  const minutes = date.getMinutes();
+  const ampm = date.getHours() >= 12 ? "PM" : "AM";
+
+  const formattedDate = `${month} ${day}, ${year}, ${hours}:${
+    minutes < 10 ? "0" : ""
+  }${minutes} ${ampm}`;
+
+  return formattedDate;
+};
