d78963e241ac10bb6f86d418a77564242f6b8b9e
Ossama kharbaq <ossamakharbaq4@gmail.com>
2024-03-11 17:01:33 +0000

    SPRIN-4: Add UncheckedBox component for project creation

---
+++ Client/src/components/icons/UncheckedBox.jsx
@@ -0,0 +1,24 @@
+const UncheckedBox = ({ className, color }) => {
+  return (
+    <svg
+      className={className}
+      width="15"
+      height="15"
+      viewBox="0 0 15 15"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect
+        x="1"
+        y="1"
+        width="13"
+        height="13"
+        rx="2"
+        stroke={color || "#5577FF"}
+        stroke-width="2"
+      />
+    </svg>
+  );
+};
+
+export default UncheckedBox;
