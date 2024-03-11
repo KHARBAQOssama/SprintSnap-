7a3039b635ec2b8153ee241242d9b076120aef86
Ossama kharbaq <ossamakharbaq4@gmail.com>
2024-03-11 17:01:31 +0000

    SPRIN-4: Update dashboard project item for project creation

--- Client/src/components/dashboard/sidebar/ProjectItem.jsx
+++ Client/src/components/dashboard/sidebar/ProjectItem.jsx
@@ -1,11 +1,22 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const ProjectItem = ({ project }) => {
+  const navigate = useNavigate();
+  const handleClick = (_id) => {
+    console.log(_id);
+    navigate(`/dashboard/projects/${_id}`);
+  };
   return (
-    <li className="flex gap-3 items-center">
+    <li
+      className="flex gap-3 items-center hover:bg-blue-100 cursor-pointer p-1 rounded-md"
+      onClick={() => handleClick(project._id)}
+    >
       <img className="h-8 w-8" src={project.icon} alt="" />
       <span className="text-sm">{project.name}</span>
-      <span className="text-[#9896A3] text-sm ms-auto font-semibold">{project.analytics}</span>
+      <span className="text-[#9896A3] text-sm ms-auto font-semibold">
+        {project.analytics}
+      </span>
     </li>
   );
 };
