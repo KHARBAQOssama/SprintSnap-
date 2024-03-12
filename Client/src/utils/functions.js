46d641531a128ea2c3b85ab14e33999311af131a
Ossama kharbaq <ossamakharbaq4@gmail.com>
2024-03-12 16:11:57 +0000

    SPRIN-4: Update utility functions for project creation

--- Client/src/utils/functions.js
+++ Client/src/utils/functions.js
@@ -6,11 +6,7 @@ export const randomColorGenerator = () => {
   const string = "12345678ABCDEF";
   let color = "#";
   for (let i = 0; i < 6; i++) {
-    // console.log(Math.floor(Math.random * 13));
-    // console.log(string[Math.floor(Math.random() * 13)]);
-    // console.log(string[0]);
     color += string[Math.floor(Math.random() * 13)];
-    console.log(color);
   }
   return color;
 };
