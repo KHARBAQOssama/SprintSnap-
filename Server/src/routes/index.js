116c0d6fc932ebdb63c15e4bbbfaae827d85d4ba
Ossama kharbaq <ossamakharbaq4@gmail.com>
2024-03-12 16:11:57 +0000

    SPRIN-4: Update routes for project creation and inviting collaborators

--- Server/src/routes/index.js
+++ Server/src/routes/index.js
@@ -2,10 +2,14 @@ const express = require("express");
 const authRouter = require('./auth.router')
 const userRouter = require('./user.router')
 const projectRouter = require('./project.router')
+const teamRouter = require('./team.router')
+const invitationRouter = require('./invitation.router')
 const router = express.Router();
 
 router.use('/auth', authRouter);
 router.use('/user', userRouter);
+router.use('/team', teamRouter);
+router.use('/invitation', invitationRouter);
 router.use('/project', projectRouter);
 
 module.exports = router;
