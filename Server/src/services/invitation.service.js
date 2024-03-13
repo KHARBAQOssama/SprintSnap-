13b8009639e103f4e7d693592736c3f703894cfe
Ossama kharbaq <ossamakharbaq4@gmail.com>
2024-03-13 13:29:07 +0000

    SPRIN-4: Update invitation service for invitation confirmation

--- Server/src/services/invitation.service.js
+++ Server/src/services/invitation.service.js
@@ -1,3 +1,5 @@
+const teamControllerInstance = require("../controllers/team.controller");
+
 class InvitationService {
   model;
   constructor(Model) {
@@ -16,6 +18,48 @@ class InvitationService {
     return await this.model.findOne({
       $and: [{ user: searchParams.user }, { project: searchParams.project }],
     });
+  };
+  getById = async (id) => {
+    return await this.model
+      .findById(id)
+      .populate({
+        path: "user",
+        model: "User",
+        select: "-password -refreshToken -__v ",
+      })
+      .populate({
+        path: "by",
+        model: "User",
+        select: "-password -refreshToken -__v ",
+      })
+      .populate({
+        path: "project",
+        model: "Project",
+        select: "-description -task_status -status -team -deleted -__v ",
+      });
+  };
+  confirm = async (id) => {
+    try {
+      const invitation = await this.model.findById(id).populate({
+        path: "project",
+        model: "Project",
+        populate: {
+          path: "team",
+          model: "Team",
+        },
+      });
+      if (!invitation) {
+        throw new Error("Invitation not found");
+      }
+      const team = await teamControllerInstance.insertOne(
+        invitation.project.team,
+        invitation.user
+      );
+      await this.model.findByIdAndDelete(id);
+      return team;
+    } catch (error) {
+      throw error;
+    }
   };
   getByProject = async (project) => {
     return await this.model.find({ project });
