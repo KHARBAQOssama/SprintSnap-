f8a5bb2dded037fcba7bd65f94be20f35a7be30d
Ossama kharbaq <ossamakharbaq4@gmail.com>
2024-03-13 13:29:07 +0000

    SPRIN-4: Update team router for invitation confirmation

--- Server/src/routes/team.router.js
+++ Server/src/routes/team.router.js
@@ -3,6 +3,5 @@ const teamControllerInstance = require('../controllers/team.controller');
 const { requireAuth } = require('../middlewares');
 const router = express.Router();
 
-router.post('/:id/invite', requireAuth, teamControllerInstance.inviteToCollaboration)
 
 module.exports = router
