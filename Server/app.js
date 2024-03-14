b959c5564d4ae9db0c061f088cc5a0437dd03793
Ossama kharbaq <ossamakharbaq4@gmail.com>
2024-03-14 15:17:51 +0000

    SPRIN-4: Implement sockets for real-time notifications - Update server files

--- Server/app.js
+++ Server/app.js
@@ -1,7 +1,8 @@
-const express = require('express');
+const express = require("express");
+const http = require("http");
 const app = express();
 const cookieParser = require("cookie-parser");
-const routes = require('./src/routes')
+const routes = require("./src/routes");
 const cors = require("cors");
 
 require("dotenv").config();
@@ -17,9 +18,45 @@ app.use(
   })
 );
 
+const socketIo = require("socket.io");
+const { handleSocketConnection } = require("./src/config/socketHandler");
+const { initializeSocket } = require("./src/config/socket.config");
+
+const server = http.createServer(app);
+const io = initializeSocket(server);
+app.set("socketIo", io);
+// const io = socketIo(server, {
+//   cors: {
+//     origin: "http://localhost:5173",
+//   },
+// });
+
+// const userSocketMap = new Map();
+
+// io.on("connection", (socket) => {
+  
+
+//   socket.on("authenticate", (userId) => {
+//     userSocketMap.set(userId, socket.id);
+//   });
+
+//   socket.on("disconnect", () => {
+//     console.log("Client disconnected: " + socket.id);
+
+//     userSocketMap.forEach((value, key) => {
+//       if (value === socket.id) {
+//         userSocketMap.delete(key);
+//       }
+//     });
+//   });
+// });
+io.on("connection", handleSocketConnection);
+
+app.set("socketio", io);
+
 app.use("/api", routes);
 
-const PORT = process.env.PORT || 3000
-app.listen(PORT, ()=>{
-    console.log(`running on ${PORT}`);
-})
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`running on ${PORT}`);
+});
