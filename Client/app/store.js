4dba47efd3bddfaa76e6cdc9cb121f902dae298e
Ossama kharbaq <ossamakharbaq4@gmail.com>
2024-03-16 15:58:16 +0000

    SPRIN-18: Notification implementation - Update store and Listener component

--- Client/app/store.js
+++ Client/app/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from '../features/auth/slice'
 import projectReducer from '../features/project/slice'
+import notificationReducer from '../features/notification/slice'
 import appStatusReducer from '../features/appStatus/slice'
 
 export const store = configureStore({
     reducer : {
         auth: authReducer,
         project: projectReducer,
+        notification: notificationReducer,
         appStatus: appStatusReducer,
     }
 })
