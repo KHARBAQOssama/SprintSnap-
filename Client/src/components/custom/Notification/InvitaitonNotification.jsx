c220048b383293894a93a4bf0a5ebc08d7cd625a
Ossama kharbaq <ossamakharbaq4@gmail.com>
2024-03-18 13:12:28 +0000

    SPRIN-18: Notification implementation - Add InvitationNotification component

--- Client/src/components/custom/Notification/InvitaitonNotification.jsx
+++ Client/src/components/custom/Notification/InvitaitonNotification.jsx
@@ -1,9 +1,14 @@
+import { Link } from "react-router-dom";
+import { formatTimestamp } from "../../../utils/functions";
 import QuestionIcon from "../../icons/QuestionIcon";
 
 const InvitationNotification = ({ notification }) => {
   console.log(notification.context);
   return (
-    <div className="p-2 bg-blue-50 hover:bg-blue-100 rounded-lg cursor-pointer flex flex-col gap-2">
+    <Link
+      to={`/invitation/${notification.context._id}/confirm`}
+      className="p-2 bg-blue-50 hover:bg-blue-100 rounded-lg cursor-pointer flex flex-col gap-2"
+    >
       <div className="flex items-center gap-3">
         <QuestionIcon className={`w-4 h-4 fill-blue-400`} />{" "}
         <span className="text-xs font-light text-blue-400">
@@ -29,18 +34,19 @@ const InvitationNotification = ({ notification }) => {
         <div className="flex-1 flex flex-col">
           <p className="text-xs text-gray-400">
             <span className="font-semibold text-black">
-              {notification.by.first_name}{" "}
-              {notification.by.last_name}
+              {notification.by.first_name} {notification.by.last_name}
             </span>{" "}
             invited you to his team working on the project{" "}
             <span className="text-blue-500 font-semibold">
               {notification.project.name}
             </span>
           </p>
-          <p className=" text-[10px] ms-auto">today , 12:00</p>
+          <p className=" text-[10px] ms-auto">
+            {formatTimestamp(notification.createdAt)}
+          </p>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
