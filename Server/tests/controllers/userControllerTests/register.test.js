f129c0322c74d523880c1ab1696b30eb137e4734
Ossama kharbaq <ossamakharbaq4@gmail.com>
2024-03-20 22:57:19 +0000

    SPRIN-22: Add tests for user registration to ensure reliability and security

--- Server/tests/controllers/userControllerTests/register.test.js
+++ Server/tests/controllers/userControllerTests/register.test.js
@@ -10,8 +10,8 @@ app.use(express.json());
 app.use("/auth", require("../../../src/routes/auth.router"));
 
 let email = faker.internet.email();
-let full_name = faker.person.fullName();
-let phone_number = faker.phone.number();
+let first_name = faker.person.fullName();
+let last_name = faker.person.fullName();
 
 const cookies = {
   accessToken: "the-access-token-value",
@@ -19,54 +19,36 @@ const cookies = {
 };
 
 describe("register function", () => {
-//   it("should return a 401 response if the user is logged in", async () => {
-//     const body = {};
-//     const response = await request(app)
-//       .post("/auth/register")
-//       .set(
-//         "Cookie",
-//         `accessToken=${cookies.accessToken}; refreshToken=${cookies.refreshToken}`
-//       )
-//       .send(body);
-//     expect(response.status).toBe(401);
-//     expect(response.body).toEqual({
-//       message: "Action denied, you are logged in",
-//     });
-//   });
+  it("should return a 400 response if any required field is missing", async () => {
+    const body = {};
+    const response = await request(app).post("/auth/register").send(body);
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toHaveProperty("errors");
+    expect(Array.isArray(response.body.errors)).toBeTruthy();
+  });
 
-//   it("should return a 400 response if any required field is missing", async () => {
-//     const body = {};
-//     const response = await request(app).post("/auth/register").send(body);
-//     expect(response.statusCode).toBe(400);
-//     expect(response.body).toEqual({ message: "All fields are required" });
-//   });
-
-//   it("should return a 400 response if the passwords are not the same", async () => {
-//     const body = {
-//       email,
-//       full_name,
-//       password: "password123",
-//       password_confirmation: "password456",
-//       phone_number,
-//       address: "123 Main St",
-//       role: "Client",
-//     };
-//     const response = await request(app).post("/auth/register").send(body);
-//     expect(response.statusCode).toBe(400);
-//     expect(response.body).toEqual({ message: "Passwords do not match" });
-//   });
-
+    it("should return a 400 response if the passwords are not the same", async () => {
+      const body = {
+        email,
+        first_name,
+        last_name,
+        password: "password123",
+        password_confirmation: "password456",
+      };
+      const response = await request(app).post("/auth/register").send(body);
+      expect(response.statusCode).toBe(400);
+      expect(response.body).toHaveProperty("errors");
+      expect(Array.isArray(response.body.errors)).toBeTruthy();
+    });
 
   it("should return a 409 response if Email already token", async () => {
     User.findOne = jest.fn().mockResolvedValue({ email: true });
     const body = {
       email,
-      full_name,
+      first_name,
+      last_name,
       password: "password123",
       password_confirmation: "password123",
-      phone_number,
-      address: "123 Main St",
-      role: "Client",
     };
     const response = await request(app).post("/auth/register").send(body);
     expect(response.statusCode).toBe(409);
