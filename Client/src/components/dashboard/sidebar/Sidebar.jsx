95c00e40f633ddc925dad13b9277f2a437857580
Ossama kharbaq <ossamakharbaq4@gmail.com>
2024-03-20 15:51:36 +0000

    Update sidebar: Add new options and improve navigation in the sidebar

--- Client/src/components/dashboard/sidebar/Sidebar.jsx
+++ Client/src/components/dashboard/sidebar/Sidebar.jsx
@@ -1,52 +1,35 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import Logo from "../../common/Logo";
-import Activity from "../../icons/Activity";
-import Category from "../../icons/Category";
-import Folder from "../../icons/Folder";
-import Graph from "../../icons/Graph";
-import PlusIcon from "../../icons/PlusIcon";
-import { useEffect } from "react";
-import { getAll } from "../../../../features/project/slice";
-import ProjectItem from "./ProjectItem";
 import SidebarProjects from "./SidebarProjects";
 import sidebarItems from "../../../widgets/sidebarItems";
-import { useLocation } from "react-router-dom";
-import SidebarItem from "./SidebarItems";
 import SidebarItems from "./SidebarItems";
+import { randomColorGenerator } from "../../../utils/functions";
 
 const Sidebar = () => {
   const { user } = useSelector((state) => state.auth);
   const sideItem = sidebarItems[0];
   return (
-    <aside className="bg-white min-h-[100vh] min-w-[16em] w-[16em] p-[1em] px-[1.6em] border-r flex flex-col">
+    <aside className="bg-white min-h-[100vh] min-w-[256px] w-[16em] p-[1em] px-[1.6em] border-r flex flex-col">
       <div className="p-2 py-3 flex gap-1">
         <Logo classItems={"h-[2.5em]"} />
       </div>
       <div className="py-2">
-        {/* <ul className="flex flex-col">
-          <li className="text-[#9896A3] px-2 py-2 w-full flex items-center gap-2">
-            <Category className={"w-[1.3em] h-[1.3em]"} />
-            Overview
-          </li>
-          <li className="text-[#9896A3] px-2 py-2 w-full flex items-center gap-2">
-            <Graph className={"w-[1.3em] h-[1.3em]"} />
-            Analytics
-          </li>
-          <li className="text-[#9896A3] px-2 py-2 w-full flex items-center gap-2">
-            <Activity className={"w-[1.3em] h-[1.3em]"} />
-            Activities
-          </li>
-          <li className="text-white px-4 py-3 rounded-2xl w-full flex items-center gap-2 bg-[#5577FF]">
-            <Folder className={"w-[1.3em] h-[1.3em]"} />
-            Projects
-          </li>
-        </ul> */}
         <SidebarItems />
       </div>
       <SidebarProjects />
       <div className="border rounded-xl flex gap-2 mt-auto overflow-hidden items-center">
-        <div className="relative h-full">
-          <img className="h-full" src="/icons/defaultProfile.png" alt="" />
+        <div
+          className="relative h-full w-[48px] flex items-center justify-center"
+          style={{ backgroundColor: randomColorGenerator() }}
+        >
+          {user.image_url ? (
+            <img className="h-full" src={user.image_url} alt="" />
+          ) : (
+            <span className="text-2xl font-bold text-white">
+              {user.first_name[0]}
+              {user.last_name[0]}
+            </span>
+          )}
         </div>
         {user && (
           <div className="flex flex-col py-2">
